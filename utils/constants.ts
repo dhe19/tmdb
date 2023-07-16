c731edc1dd15799fd4dc0bdaa10c9a027907f1cc
Edu <info.die@gmail.com>
2023-07-15 21:58:09 -0600

    add constants file

---
+++ utils/constants.ts
@@ -0,0 +1,10 @@
+
+export const apiKey = "0e1ea240862f134bc46d12ff77d7564e";
+export const popularUrl = "https://api.themoviedb.org/3/movie/popular";
+export const movieUrl = "https://api.themoviedb.org/3/movie/";
+export const options: Intl.DateTimeFormatOptions = {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  };
+  export const urlImage = "https://image.tmdb.org/t/p/w500"
