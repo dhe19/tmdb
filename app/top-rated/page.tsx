ee833e254aa5f758b65af96d3ab1ccc8ce98fcae
Edu <info.die@gmail.com>
2023-07-16 22:09:03 -0600

    add two new pages

---
+++ app/top-rated/page.tsx
@@ -0,0 +1,9 @@
+import Main from "@/components/Main";
+import { topRatedUrl } from "@/utils/constants";
+import React from "react";
+
+const Page = () => {
+  return <Main url={topRatedUrl} />;
+};
+
+export default Page;
---
+++ app/upcoming/page.tsx
@@ -0,0 +1,9 @@
+import Main from "@/components/Main";
+import { upcomingUrl } from "@/utils/constants";
+import React from "react";
+
+const Page = () => {
+  return <Main url={upcomingUrl} />;
+};
+
+export default Page;
