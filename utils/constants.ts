3b11b86bbeccf2286752d4aec924640db2a74e36
Edu <info.die@gmail.com>
2023-07-16 22:06:34 -0600

    update constants

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,6 +1,8 @@
 
 export const apiKey = "0e1ea240862f134bc46d12ff77d7564e";
 export const popularUrl = "https://api.themoviedb.org/3/movie/popular";
+export const upcomingUrl = "https://api.themoviedb.org/3/movie/upcoming";
+export const topRatedUrl = "https://api.themoviedb.org/3/movie/top_rated";
 export const movieUrl = "https://api.themoviedb.org/3/movie/";
 export const options: Intl.DateTimeFormatOptions = {
     month: "short",
