6f80e2258d937691466327ca01ef3e9eeeeae077
Edu <info.die@gmail.com>
2023-07-17 06:20:43 -0600

    fix problem

--- components/Poster/molecules/Credits.tsx
+++ components/Poster/molecules/Credits.tsx
@@ -6,8 +6,8 @@ interface Props {
 const Credits: React.FC<Props> = ({ credist }) => {
   return (
     <ul className="text-center lg:text-start p-4 gap-y-5  grid grid-cols-2 lg:grid-cols-3 gap-3 items-center mt-8">
-      {credist?.map((e) => (
-        <li className="flex flex-col">
+      {credist?.map((e, i) => (
+        <li className="flex flex-col" key={i}>
           <span className="font-medium text-sm">{e?.name ?? "-"}</span>
           <span className="font-light text-sm">
             {e?.knownForDepartment ?? "-"}
