d592ef26e78f237dc47504089eea5fad9258ef43
Edu <info.die@gmail.com>
2023-07-16 22:07:28 -0600

    update navMenu

--- components/Navbar/molecules/NavMenu.tsx
+++ components/Navbar/molecules/NavMenu.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 interface Props {
@@ -6,9 +7,22 @@ interface Props {
 
 const NavMenu: React.FC<Props> = ({ navItems }) => {
   return (
-    <ul className=" hidden lg:flex items-center gap-7 text-sm">
+    <ul className=" hidden lg:flex items-center gap-7 text-sm ">
       {navItems.map((item, index) => (
-        <li key={index}>{item}</li>
+        <li key={index} className="relative group">
+          {item}
+          <ul className="hidden group-hover:block py-2 px-4 font-light z-20 bg-white absolute bottom-26 rounded-lg border border-slate-300 text-slate-950">
+            <li>
+              <Link href={"/"}>Popular</Link>
+            </li>
+            <li>
+              <Link href={"/top-rated"}>Top Rated</Link>
+            </li>
+            <li>
+              <Link href={"/upcoming"}>Upcoming</Link>
+            </li>
+          </ul>
+        </li>
       ))}
     </ul>
   );
