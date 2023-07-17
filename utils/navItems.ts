29203499e46b2047e92a140a7a1c139eb92136e5
Edu <info.die@gmail.com>
2023-07-17 03:23:18 -0600

    update navItems

--- utils/navItems.ts
+++ utils/navItems.ts
@@ -1,1 +1,71 @@
-export const navItems = ['Movies', 'TV Shows','People', 'More']
+export const navItems: Array<NavItem> = [
+    {
+        title: 'Movies',
+        subLinks: [
+            {
+                name: 'Popular',
+                path: '/'
+            }
+            ,
+            {
+                name: 'Upcoming',
+                path: '/upcoming'
+            }
+            ,
+            {
+                name: 'Top Rate',
+                path: '/top-rated'
+            }]
+    },
+
+    {
+        title: 'TV Shows',
+        subLinks: [
+            {
+                name: 'Popular',
+                path: '/'
+            },
+            {
+                name: 'Airing Play',
+                path: '/'
+            },
+            {
+                name: 'On TV',
+                path: '/'
+            },
+            {
+                name: 'Top Rated',
+                path: '/'
+            },]
+    },
+
+    {
+        title: 'Pople',
+        subLinks: [
+            {
+                name: 'Popular People',
+                path: '/'
+            }]
+    },
+
+    {
+        title: 'More',
+        subLinks: [
+            {
+                name: 'Discussions',
+                path: '/'
+            },
+            {
+                name: 'Leaderboard',
+                path: '/'
+            },
+            {
+                name: 'Support',
+                path: '/'
+            },
+            {
+                name: 'API',
+                path: '/'
+            },]
+    },
+]
